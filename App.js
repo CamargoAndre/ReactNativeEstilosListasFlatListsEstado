32a32d28111852847075d250de76046f6a5d856a
Andre de Camargo <camargo_and@hotmail.com>
2020-03-19 22:31:06 -0300

    Editando Estilo

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      <View><Text style={styles.titulo}>Cadastrar Contatos</Text></View>
+      <View style={styles.cadastroContato}>
+        <TextInput style={styles.cadastroImputText} placeholder="Nome" />
+        <TextInput style={styles.cadastroImputText} placeholder="Telefone" />
+        <Button 
+          title="Adicionar"
+        />
+
+      </View>
+      <View>
+        <View><Text style={styles.titulo}>Lista de Contatos</Text></View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:50,
+    alignItems: 'center'
   },
+  
+  titulo: {
+    fontSize: 30,
+    marginBottom:20
+  },
+
+  cadastroContato: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+
+  cadastroImputText: {
+    fontSize:20,
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    marginBottom:5,
+    padding: 1
+
+  }
+
+
 });
